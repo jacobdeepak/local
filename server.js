53f83f44c5696f1da37ee3d6e2525b4e2cb49ac4
jacobdeepak <deepak.jacob@telus.com>
2017-12-07 16:26:19 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(express.static("public"));
+app.listen(port, () => console.log("TDally running on port", port));
